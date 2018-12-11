efc401c80da8eb23634f3b440a497605b5dc57f7
huangshuai <shuai.huang3@pactera.com>
2018-12-11 14:42:17 +0800

    modiffy

---
+++ src/main/java/com/pactera/spring/interceptor/MyBatisInterceptor.java
@@ -0,0 +1,36 @@
+package com.pactera.spring.interceptor;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.*;
+
+import java.util.Properties;
+
+@Intercepts({
+        @Signature(
+                type = Executor.class,
+                method = "update",
+                args = {MappedStatement.class, Object.class}
+        )
+})
+public class MyBatisInterceptor implements Interceptor {
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = invocation.proceed();
+        long end = System.currentTimeMillis();
+        MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
+        System.out.println(mappedStatement.getId() + "耗时:" + (end - start));
+        return result;
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return Plugin.wrap(target, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
