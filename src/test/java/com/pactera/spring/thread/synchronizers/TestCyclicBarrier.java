f1dedbbc7d9ff5ee5fba2081036ae53db0cda2cd
huangshuai <shuai.huang3@pactera.com>
2018-12-19 17:30:20 +0800

    modiffy

--- src/test/java/com/pactera/spring/thread/synchronizers/TestCyclicBarrier.java
+++ src/test/java/com/pactera/spring/thread/synchronizers/TestCyclicBarrier.java
@@ -9,15 +9,20 @@ import java.util.concurrent.CyclicBarrier;
  * 1.CyclicBarrier在创建的时候指定一个count值，当该CyclicBarrier的await()方法被调用的次数等于count值
  * 并且调用该方法的线程也执行到该方法处后，如果全部的调用await()方法都执行到了该处，则进行后续的执行；
  * 2.也可以在创建CyclicBarrier的时候指定一个barrierAction，当所有的await()方法都执行到的时候，该barrierAction
- * 就会被执行；
+ * 就会被执行,但是barrierAction没有执行完之前其他await的线程是会被阻塞的，不能继续执行；
  * 3.如果await()方法的调用次数小于CyclicBarrier的count值，则会一直等待；
  * 4.当所有的waiting线程都已经释放之后，我们可以继续使用CyclicBarrier
  */
 public class TestCyclicBarrier {
     public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
         CyclicBarrier c = new CyclicBarrier(2,()->{
-            System.out.println("kkkkkkkkkk");
-            System.out.println(new Date());
+            try {
+                Thread.sleep(1000);
+                System.out.println(new Date());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("barrierAction " + new Date());
         });
         new Thread(() -> {
             System.out.println("1a");
@@ -28,26 +33,24 @@ public class TestCyclicBarrier {
             } catch (BrokenBarrierException e) {
                 e.printStackTrace();
             }
+            try {
+                Thread.sleep(30000);
+                System.out.println(" 111" + new Date());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("1b");
 
         }).start();
         System.out.println("getParties:" + c.getNumberWaiting());
         System.out.println("getParties:" + c.getParties());
         c.await();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("2a");
-                    /*try {
-                        c.await();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (BrokenBarrierException e) {
-                        e.printStackTrace();
-                    }*/
-                System.out.println("2b");
-            }
-        }).start();
+        try {
+            Thread.sleep(1000);
+            System.out.println(" 222" + new Date());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("getParties:" + c.getNumberWaiting());
         System.out.println(3);
     }
